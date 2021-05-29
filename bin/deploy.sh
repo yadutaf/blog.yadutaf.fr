25535a8c4a4b2bec47c67420000beea565d706d3
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2021-05-29 11:10:59 +0200

    Remove generated pages from git
    Signed-off-by: Jean-Tiare Le Bigot <jt@yadutaf.fr>

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-ssh root@xeon.yadutaf.fr -- mkdir -m 0755 -p /var/www
-rsync -avz --chown www-data:www-data public/ root@xeon.yadutaf.fr:/var/www/blog.yadutaf.fr
+set -e
 
+# Generate
+hugo
+
+# Upload
+rsync -avz --chown www-data:www-data public/ root@xeon.yadutaf.fr:/var/www/blog.yadutaf.fr
